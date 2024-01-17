490f65cb95dbfc1d093529536d1926d5fcb45ab8
harry38444 <harry38444@gmail.com>
2024-01-17 10:21:56 -0500

    a

--- app.py
+++ app.py
@@ -8,25 +8,40 @@ elif int(randomnumber) == 2:
     response = "Paper"
 elif int(randomnumber) == 3:
     response = "Scissors"
-Input = input("Rock Paper or Scissors?: ")
-if Input == 1 and response == "Paper":
-    print("You lose")
-    list.append("win")
-elif Input == 3 and response == "Paper":
-    print("You won")
-    list.append("win")
-elif Input == 1 and response == "Scissors":
-    print("You won")
-    list.append("win")
-elif Input == 2 and response == "Scissors":
-    print("You lost")
-    list.append("lose")
-elif Input == 2 and response == "Rock":
-    print("You won")
-    list.append("win")
-elif Input == 3 and response == "Rock":
-    print("You lost")
-    list.append("lose")
-elif Input == response:
-    print("You draw")
-    list.append("draw")
+def rockpaperscissorsgame():
+    Input = input("Rock, Paper or Scissors?: ")
+    if Input == "Rock" and response == "Paper":
+        print("Opponent chose:", response)
+        print("You lost")
+        list.append("loss")
+    elif Input == "Scissors" and response == "Paper":
+        print("Opponent chose:", response)
+        print("You won")
+        list.append("win")
+    elif Input == "Rock" and response == "Scissors":
+        print("Opponent chose:", response)
+        print("You won")
+        list.append("win")
+    elif Input == "Paper" and response == "Scissors":
+        print("You lost")
+        list.append("loss")
+    elif Input == "Paper" and response == "Rock":
+        print("Opponent chose:", response)
+        print("You won")
+        list.append("win")
+    elif Input == "Scissors" and response == "Rock":
+        print("Opponent chose:", response)
+        print("You lost")
+        list.append("loss")
+    elif Input == response:
+        print("Opponent chose:", response)
+        print("You drawed")
+        list.append("draw")
+    else: 
+        print("Error, check spelling and write the first letter in capital")
+
+continue_game = input("Would you like to continue the game? Yes/No:")
+while continue_game.upper() == "Yes":
+    rockpaperscissorsgame()
+    continue_game = input("Would you like to continue the game? Yes/No:")
+print("Statistics:", list) 
